179438e0094b1d3bd45dcefa6ffa0f963758f4b7
Matt Jones <matt@scalybones.com>
2026-07-14 21:26:49 -0500

    frontend to backend search added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,36 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 function App() {
   const [players, setPlayers] = useState<any[]>([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
-  useEffect(() => {
-    fetch("http://127.0.0.1:8000/players")
-      .then((res) => res.json())
-      .then((data) => setPlayers(data))
-      .catch((error) => {
-        console.error("API error:", error);
-      });
-  }, []);
+
+  function searchPlayers(){
+    fetch(`http://127.0.0.1:8000/players?name=${encodeURIComponent(searchTerm)}`)
+    .then((res) => res.json())
+    .then((data) => setPlayers(data))
+    .catch((error) => {
+      console.error("API error:", error);
+    });
+  }
 
   return (
     <main style={{ padding: "2rem", fontFamily: "Arial" }}>
       <h1>AI Fantasy Coach</h1>
       <p>This is the AI powered coach for Fantasy</p>
+
+      <div>
+        <h2>Search Player</h2>
+
+        <input 
+          value = {searchTerm}
+          placeholder="Type here"
+          onChange={(event) => {
+            setSearchTerm(event.target.value)
+          }}
+        />
+        <button onClick={searchPlayers}>Search</button>
+      </div>
 
       <div
         style={{
